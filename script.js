2c4b3f8470de9890100392eec4716a9dea6800fa
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-06-06 19:09:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,56 +1,54 @@
 let circles = document.querySelectorAll(".circle");
 let lines = document.querySelectorAll(".line");
-let nextBtn = document.querySelector("#next")
+let nextBtn = document.querySelector("#next");
 let prevBtn = document.querySelector("#prev");
 
 let currentActive = 1;
 
-
-nextBtn.addEventListener("click", ()=>{
-     currentActive++;
-     if(currentActive > circles.length){
-            currentActive = circles.length;
-     }
-
-     ui_Update()
-})
-
-prevBtn.addEventListener("click" , ()=>{
-    currentActive--;
-    if(currentActive < 1){
-         currentActive = 1;
+nextBtn.addEventListener("click", () => {
+    currentActive++;
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
     }
 
-    ui_Update()
-})
+    ui_Update();
+});
 
-function ui_Update(){
-    circles.forEach((circle, index)=>{
-           if(index < currentActive){
-                circle.classList.add("active")
-           }
-           else{
-               circle.classList.remove("active")
-           }
-    })
+prevBtn.addEventListener("click", () => {
+    currentActive--;
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
 
-    lines.forEach((line, index)=>{
-          if(index < currentActive-1){
-                line.classList.add("active")
-          }
-          else{
-             line.classList.remove("active")
-          }
-       })
+    ui_Update();
+});
 
-       if(currentActive === 1){
-           prevBtn.disabled = true;
-      }
-      else if(currentActive === circles.length){
-          nextBtn.disabled = true;
-      }
-      else{
-            nextBtn.disabled = false
-            prevBtn .disabled = false
-      }
+function ui_Update() {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add("active");
+        } else {
+            circle.classList.remove("active");
+        }
+    });
+
+    lines.forEach((line, index) => {
+        if (index < currentActive - 1) {
+            line.classList.add("active");
+        } else {
+            line.classList.remove("active");
+        }
+    });
+
+    if (currentActive === 1) {
+        prevBtn.disabled = true;
+    } else if (currentActive === circles.length) {
+        nextBtn.disabled = true;
+    } else {
+        nextBtn.disabled = false;
+        prevBtn.disabled = false;
+    }
 }
+
+ui_Update();
+
