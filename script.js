d46bce54f4e9f8ca778c2ca8a667d0dd37eac83b
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-06-06 17:28:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-//your JS code here. If required.
+let circles = document.querySelectorAll(".circle");
+let lines = document.querySelectorAll(".line");
+let nextBtn = document.querySelector("#next")
+let prevBtn = document.querySelector("#prev");
+
+let currentActive = 1;
+
+
+nextBtn.addEventListener("click", ()=>{
+     currentActive++;
+     if(currentActive > circles.length){
+            currentActive = circles.length;
+     }
+
+     ui_Update()
+})
+
+prevBtn.addEventListener("click" , ()=>{
+    currentActive--;
+    if(currentActive < 1){
+         currentActive = 1;
+    }
+
+    ui_Update()
+})
+
+function ui_Update(){
+    circles.forEach((circle, index)=>{
+           if(index < currentActive){
+                circle.classList.add("active")
+           }
+           else{
+               circle.classList.remove("active")
+           }
+    })
+
+    lines.forEach((line, index)=>{
+          if(index < currentActive-1){
+                line.classList.add("active")
+          }
+          else{
+             line.classList.remove("active")
+          }
+       })
+
+       if(currentActive === 1){
+           prevBtn.disabled = true;
+      }
+      else if(currentActive === circles.length){
+          nextBtn.disabled = true;
+      }
+      else{
+            nextBtn.disabled = false
+            prevBtn .disabled = false
+      }
+}
